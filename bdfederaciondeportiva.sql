eed877fb6367e2f4c5275db5b6744ca0e34a0ee6
manuelmc09 <manuelmc09@educastur.es>
2022-01-25 20:54:48 +0100

    Base de datos corregida.Falta añadir más datos en la tabla atletas

--- bdfederaciondeportiva.sql
+++ bdfederaciondeportiva.sql
@@ -1,131 +1,140 @@
--- MySQL Workbench Forward Engineering
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
--- -----------------------------------------------------
--- Schema bdfederaciondeportiva
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema bdfederaciondeportiva
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `bdfederaciondeportiva` DEFAULT CHARACTER SET utf8 ;
-USE `bdfederaciondeportiva` ;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `bdfederaciondeportiva`
+-- Base de datos: `bdfederacion`
 --
--- CREATE DATABASE IF NOT EXISTS `bdfederaciondeportiva` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
--- USE `bdfederaciondeportiva`;
+CREATE DATABASE IF NOT EXISTS `bdfederacion` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `bdfederacion`;
+
 
 -- --------------------------------------------------------
--- -----------------------------------------------------
--- Table `bdfederaciondeportiva`.`persona`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `bdfederaciondeportiva`.`persona`;
-CREATE TABLE IF NOT EXISTS `bdfederaciondeportiva`.`persona` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(50) NOT NULL,
-  `telefono` VARCHAR(15) NOT NULL,
-  `fechanac` DATE NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
 
+--
+-- Estructura de tabla para la tabla `persona`
+--
 
+DROP TABLE IF EXISTS `persona`;
+CREATE TABLE IF NOT EXISTS `persona` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `telefono` varchar(15) NOT NULL,
+  `fechanac` date NOT NULL,
+  PRIMARY KEY (`id`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  
+  
 --
 -- Volcado de datos para la tabla `persona`
 --
 
 INSERT INTO `persona` (`id`, `nombre`, `telefono`, `fechanac`) VALUES
-(1,'Eli Pinedo' , '648342199','1981-05-13'),
-(2,'Gisela Pulido','677212341','1994-01-14'),
-(3,'Ana Peleteiro','+64622455621','1995-12-01'),
-(4,'Raúl Entrerrios','984562313','1981-02-12'),
-(5,'Nina Khivanevskaya','+6458623221','1978-06-24'),
-(6,'Mireia Belmonte','648234567','1990-11-10'),
-(7,'Mikkel Hansen','+64456342101','1987-10-22');
--- -----------------------------------------------------
--- Table `bdfederaciondeportiva`.`manager`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `bdfederaciondeportiva`.`manager`;
-CREATE TABLE IF NOT EXISTS `bdfederaciondeportiva`.`manager` (
-  `email` VARCHAR(50) NOT NULL,
-  `id` INT NOT NULL,
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `fk_manager_persona`
-    FOREIGN KEY (`id`)
-    REFERENCES `bdfederaciondeportiva`.`persona` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+(1, 'Adrian Valdes Garcia','622567011','2001-01-01'),
+(2,'Mónica García Solís','648454561','2000-02-08'),
+(3,'Fernando Vallejo Najera','983452331','1992-04-10'),
+(4,'Martina Villa Vivanco','921345488','1983-01-11'),
+(5,'Marta Etxebarria Ruiz','674563211','1988-05-09'),
+(6,'Eli Pinedo Saénz' , '648342199','1981-05-13'),
+(7,'Gisela Pulido Borrell','677212341','1994-01-14'),
+(8,'Ana Peleteiro Brión','622455621','1995-12-01'),
+(9,'Raúl Entrerrios Rodriguez','984562313','1981-02-12'),
+(10,'Nina Khivanevskaya','645862322','1978-06-24'),
+(11,'Mireia Belmonte García','648234567','1990-11-10'),
+(12,'Mikkel Hansen','656342101','1987-10-22');
 
+
+--
+-- Estructura de tabla para la tabla `manager`
+--
+
+DROP TABLE IF EXISTS `manager`;
+CREATE TABLE IF NOT EXISTS `manager` (
+`id` int(11) NOT NULL,
+`direccion` varchar(50) NOT NULL,
+`idpersona`int(11) NOT NULL,
+PRIMARY KEY (`id`),
+KEY `FK_PERSONA` (`idpersona`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `manager`
 --
 
-INSERT INTO `manager` (`id`, `email`) VALUES
-(1,'elipinedo@gmail.com'),
-(7,'mikkelhansen@gmail.com');
+INSERT INTO `manager`(`id`,`direccion`,`idpersona`) VALUES
+(1,'Rambla PobleNou 171 BCN',7),
+(2,'Avenida Constitución 110 Gijón',9);
 
--- -----------------------------------------------------
--- Table `bdfederaciondeportiva`.`atleta`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `bdfederaciondeportiva`.`atleta`;
-CREATE TABLE IF NOT EXISTS `bdfederaciondeportiva`.`atleta` (
-  `altura` FLOAT NOT NULL,
-  `peso` FLOAT NOT NULL,
-  `id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_atleta_persona1_idx` (`id` ASC) VISIBLE,
-  CONSTRAINT `fk_atleta_persona1`
-    FOREIGN KEY (`id`)
-    REFERENCES `bdfederaciondeportiva`.`persona` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+--
+-- Filtros para la tabla `manager`
+--
 
+ALTER TABLE `manager`
+ADD CONSTRAINT `FK_PERSONA` FOREIGN KEY (`idpersona`) REFERENCES `persona` (`id`);
+
+--
+-- Estructura de tabla para la tabla `manager`
+--
+
+DROP TABLE IF EXISTS `atleta`;
+CREATE TABLE IF NOT EXISTS `atleta` (
+`id` int(11) NOT NULL,
+`altura` float NOT NULL,
+`peso` float NOT NULL,
+`idpersona`int(11) NOT NULL,
+`idequipo` int(11) NOT NULL,
+PRIMARY KEY (`id`),
+KEY `FKPERSONA` (`idpersona`),
+KEY `FKEQUIPO` (`idequipo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `atleta`
 --
 
-INSERT INTO `atleta` (`id`, `altura`, `peso`) VALUES
-(1,,1.66,51),
-(2,1.71,52),
-(3,1.93,88);
+INSERT INTO `atleta` (`id`,`altura`,`peso`,`idpersona`,`idequipo`) VALUES
+(1,1.80,77.8,1,1),
+(2,1.63,55.3,2,1),
+(3,1.82,80.1,3,2);
 
--- -----------------------------------------------------
--- Table `bdfederaciondeportiva`.`equipo`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `bdfederaciondeportiva`.`equipo`;
-CREATE TABLE IF NOT EXISTS `bdfederaciondeportiva`.`equipo` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `añoinscripcion` DATE NOT NULL,
-  `manager_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_equipo_manager1_idx` (`manager_id` ASC) VISIBLE,
-  CONSTRAINT `fk_equipo_manager1`
-    FOREIGN KEY (`manager_id`)
-    REFERENCES `bdfederaciondeportiva`.`manager` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+--
+-- Filtros para la tabla `atleta`
+--
 
+ALTER TABLE `atleta`
+ADD CONSTRAINT `FKPERSONA` FOREIGN KEY (`idpersona`) REFERENCES `persona` (`id`),
+ADD CONSTRAINT `FKEQUIPO` FOREIGN KEY (`idequipo`) REFERENCES `equipo` (`id`);
+
+
+--
+-- Estructura de tabla para la tabla `equipo`
+--
+
+DROP TABLE IF EXISTS `equipo`;
+CREATE TABLE IF NOT EXISTS `equipo` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`anioincripcion` YEAR NOT NULL,
+`idmanager` int(11) NOT NULL,
+PRIMARY KEY (`id`),
+KEY `FKMANAGER` (`idmanager`)
+) ENGINE=InnoDB DEFAULT CHARSET= utf8mb4;
 
 --
 -- Volcado de datos para la tabla `equipo`
 --
 
-INSERT INTO `equipo` (`id`, `añoinscripcion`, `manager_id`) VALUES
-(1,'2018-02-03',1),
-(2,'2017-12-04',7);
+INSERT INTO `equipo`(`id`,`anioincripcion`,`idmanager`) VALUES
+(1,1999,1),
+(2,2001,2);
+--
+-- Filtros para la tabla `equipo`
+--
 
-COMMIT;
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+ALTER TABLE `equipo`
+ADD CONSTRAINT `FKMANAGER` FOREIGN KEY (`idmanager`) REFERENCES `manager` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
